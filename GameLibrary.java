c89541ae5e039ff159d5ddef685872af673de98c
sar08ben-design <sar08ben@gmail.com>
2026-04-28 14:43:01 -0400

    library update

--- GameLibrary.java
+++ GameLibrary.java
@@ -1,12 +1,17 @@
+import java.util.HashSet;
 
 
 public class GameLibrary
 {
         
+    public HashSet<Game> library;   
+    
     public GameLibrary()
     {
-       
+       library= new HashSet<>();
     }
 
-    
+    public void addGame(Game g){
+        library.add(g);
+    }
 }
