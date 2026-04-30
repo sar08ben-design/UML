8c87f057da50a34fa6d76bd98359f63546006cf5
%username% <patelanuj452@gmail.com>
2026-04-30 19:07:22 -0400

    Changes

--- Game.java
+++ Game.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 
+<<<<<<< Updated upstream
 public abstract class Game {
 
     private AgeRating ageRating;
@@ -48,4 +49,89 @@ public abstract class Game {
     public String getDetail() {
         return detail; 
     }
+=======
+public class Game {
+
+    private String title;
+    private Genre genre;
+    private AgeRating ageRating;
+    private Platform platform;
+    
+    private double userRating;     
+    private boolean inProgress;
+
+    public Game(String title, Genre genre, AgeRating ageRating,
+                Platform platform, double userRating) {
+        this.title = title;
+        this.genre = genre;
+        this.ageRating = ageRating;
+        this.platform = platform;
+        this.userRating = userRating;
+        this.inProgress = false;
+    }
+
+    public void startGame() {
+        inProgress = true;
+        System.out.println("Starting game: " + title);
+    }
+
+    public void showDetail() {
+        System.out.println("Title: " + title);
+        System.out.println("Genre: " + genre);
+        System.out.println("Age rating: " + ageRating);
+        System.out.println("Platform: " + platform);
+        System.out.println("User rating: " + userRating);
+        System.out.println("In progress: " + inProgress);
+    }
+
+    public void updateRating(double newRating) {
+        if (newRating < 0 || newRating > 5) {
+            System.out.println("Invalid rating (0–5).");
+            return;
+        }
+        this.userRating = newRating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public AgeRating getAgeRating() {
+        return ageRating;
+    }
+
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    public double getUserRating() {
+        return userRating;
+    }
+    
+    public String getName() {   
+        return title;           
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+        if (!(o instanceof Game)){
+            return false;
+        }
+        Game other = (Game) o;
+        return title.equalsIgnoreCase(other.title)
+               && platform == other.platform;
+    }
+
+    @Override
+    public int hashCode() {
+        return title.toLowerCase().hashCode() * 31 + platform.hashCode();
+    }
+>>>>>>> Stashed changes
 }
--- Online.java
+++ Online.java
@@ -2,9 +2,18 @@
 
 public class Online extends MultiPlayer
 {
+<<<<<<< Updated upstream
     
     public Online()
     {
       
+=======
+    public Online(String title, Genre genre, AgeRating ageRating,
+                        Platform platform, double userRating,
+                        int seed, boolean saveWorld, int level, String difficulty)
+    {
+        super(title, genre, ageRating, platform, userRating);
+>>>>>>> Stashed changes
     }
+    
 }
