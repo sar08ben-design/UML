c61c235a589b6462d9991e91b8140f7758f55d57
%username% <patelanuj452@gmail.com>
2026-05-01 15:55:34 -0400

    Game.class

--- Game.java
+++ Game.java
@@ -1,55 +1,5 @@
 import java.util.HashSet;
 
-<<<<<<< Updated upstream
-public abstract class Game {
-
-    private AgeRating ageRating;
-    private Genre genre;
-    private Platform platform;
-    private String name;
-    private int gameId;
-    private String developer;
-    private String detail;
-
-    public Game(AgeRating ageRating, Genre genre, Platform platform,
-               String name, int gameId, String developer, String detail) {
-        this.ageRating = ageRating;
-        this.genre = genre;
-        this.platform = platform;
-        this.name = name;
-        this.gameId = gameId;
-        this.developer = developer;
-        this.detail = detail;
-    }
-
-    public String getName() {
-        return name; 
-    }
-    
-    public int getGameId() {
-        return gameId; 
-    }
-    
-    public AgeRating getAgeRating() { 
-        return ageRating; 
-    }
-    
-    public Genre getGenre() { 
-        return genre; 
-    }
-    
-    public Platform getPlatform() {
-        return platform; 
-    }
-    
-    public String getDeveloper() {
-        return developer; 
-    }
-
-    public String getDetail() {
-        return detail; 
-    }
-=======
 public class Game {
 
     private String title;
@@ -133,5 +83,4 @@ public class Game {
     public int hashCode() {
         return title.toLowerCase().hashCode() * 31 + platform.hashCode();
     }
->>>>>>> Stashed changes
 }
