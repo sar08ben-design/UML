4e9853b1d5d03415df276be9d9063083b28821cc
%email% <sar08ben@gmail.com>
2026-05-04 07:13:03 -0400

    multiplayer/online/local

--- MultiPlayer.java
+++ MultiPlayer.java
@@ -1,15 +1,61 @@
-
+import java.util.HashSet;
 
 public class MultiPlayer extends Game
 {
+    private int partyMembers;
+    private String playerId;
+    private int maxPlayer;
+    private String hostName;
+    
+    private HashSet<String> playerIds;
 
     public MultiPlayer(String title, Genre genre, AgeRating ageRating,
                         Platform platform, double userRating,
                         int seed, boolean saveWorld, int level, String difficulty)
     {
         super(title, genre, ageRating, platform, userRating);
-
+       
+        this.playerIds = new HashSet<>();
     }
 
+    public void listAllPlayer() {
+        System.out.println("Players in party: " + playerIds);
+    }
+
+    public void removePlayer(String id) {
+    if (playerIds.contains(id)) {
+        playerIds.remove(id);
+        partyMembers--;
+        System.out.println("Player " + id + " removed.");
+    } else {
+        System.out.println("Player " + id + " not found.");
+    }
+}
+
+    public void addPlayerId(String id) {
+        if (partyMembers >= maxPlayer) {
+            System.out.println("Party is full! Max players: " + maxPlayer);
+            return;
+        }
+        if (!playerIds.add(id)) {
+            System.out.println("Player ID " + id + " is already taken!");
+            return;
+        }
+        partyMembers++;
+        System.out.println("Player " + id + " added.");
+    }
+
+    public int getPartyMembers() { return partyMembers; }
     
+    public String getPlayerId()     { return playerId; }
+    
+    public int getMaxPlayer()    { return maxPlayer; }
+    
+    public String getHostName()  { return hostName; }
+
+    public void setPlayerId(String playerId)  { this.playerId = playerId; }
+    
+    public void setMaxPlayer(int maxPlayer)  { this.maxPlayer = maxPlayer; }
+    
+    public void setHostName(String hostName) { this.hostName = hostName; }
 }
--- Online.java
+++ Online.java
@@ -1,14 +1,39 @@
-
-
-public class Online extends MultiPlayer
-{
+public class Online extends MultiPlayer {
+    
+    private String severName;
+    private int ping;
+    private boolean connected;
 
     public Online(String title, Genre genre, AgeRating ageRating,
-                        Platform platform, double userRating,
-                        int seed, boolean saveWorld, int level, String difficulty)
+                  Platform platform, double userRating,
+                  int seed, boolean saveWorld, int level, String difficulty)
     {
-        super(title, genre, ageRating, platform, userRating);
-
+        super(title, genre, ageRating, platform, userRating,
+              seed, saveWorld, level, difficulty);
+      
     }
+
+    public void connectServer() {
+        connected = true;
+        System.out.println("Connected to server: " + severName);
+    }
+
+    public void disconnectServer() {
+        connected = false;
+        System.out.println("Disconnected from server: " + severName);
+    }
+
+    public void checkPing() {
+        System.out.println("Current ping: " + ping + " ms");
+    }
+
+    public String getSeverName()  { return severName; }
     
+    public int getPing()          { return ping; }
+    
+    public boolean isConnected()  { return connected; }
+
+    public void setSeverName(String severName) { this.severName = severName; }
+    
+    public void setPing(int ping)              { this.ping = ping; }
 }
